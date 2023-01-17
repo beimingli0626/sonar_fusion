26ef7de0e93dbc3ff3708166f77ae0964e2d17cd
mhopki <60679688+mhopki@users.noreply.github.com>
2023-01-16 19:18:14 -0500

    displays sonar octomap and tof pc2 Creates pc2 of sonar range data launch ddk for sonar pc2

--- sonar_vis/README.md
+++ sonar_vis/README.md
@@ -5,3 +5,8 @@ This repository is to visualize and postprocess bags recorded on the ddk drone.
 - `roslaunch sonar_vis ddk_rviz.launch` for starting rviz
 - `roslaunch sonar_vis sonar_tf.launch` for publishing static transform from sonar frame to drone baselink
 - `roslaunch sonar_vis octomap_mapping.launch` for starting octomap server
+
+### Usage(sonar pointcloud2)
+- `roslaunch sonar_vis ddk_rviz_spc2.launch`
+- `roslaunch sonar_vis sonar_tf_pc2.launch`
+- `roslaunch sonar_vis octomap_mapping.launch`
---
+++ sonar_vis/src/maxsonar_listener.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+#from __future__ import print_function
+
+import rospy
+import numpy as np
+import random
+import math
+import sys
+
+import tf
+import sensor_msgs
+import std_msgs
+
+import rospy
+import numpy as np
+from sensor_msgs import point_cloud2
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointField
+from std_msgs.msg import Header
+
+from sensor_msgs.msg import Range
+from std_msgs.msg import String
+from std_msgs.msg import Header
+#from std_msgs.msg import Image
+import locale
+from locale import atof
+
+import serial
+#from serial import *
+from std_msgs.msg import String
+
+pc2_list = []
+
+def publishPC2(srange):
+    fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                PointField('y', 4, PointField.FLOAT32, 1),
+                PointField('z', 8, PointField.FLOAT32, 1),
+                PointField('intensity', 12, PointField.FLOAT32, 1)]
+
+    header = Header()
+    header.frame_id = "sonar"
+    header.stamp = rospy.Time.now()
+
+    dim = 1.0
+    u, v = np.meshgrid(np.linspace(-dim/50,dim/50,200), np.linspace(-dim/50,dim/50,200))
+    x = u * srange * 8
+    y = v * srange * 8
+    z = u + srange
+    points = np.array([z,y,x,x]).reshape(4,-1).T
+
+    pc2 = point_cloud2.create_cloud(header, fields, points)
+    return pc2
+
+
+def callback(data):
+    pc2_list.append(data.range)
+
+
+def listener():
+    rospy.init_node('pc2_publisher')
+    pub = rospy.Publisher('dragonfly26/points2', PointCloud2, queue_size=100)
+    rate = rospy.Rate(10)
+    rospy.Subscriber("sonar_topic", Range, callback)
+    while not rospy.is_shutdown():
+        if len(pc2_list) > 0:
+            cloud_out = publishPC2(pc2_list[0]) 
+            pc2_list.pop(0)
+            pub.publish(cloud_out)
+        rate.sleep()
+
+
+if __name__=='__main__':
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
